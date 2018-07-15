3bc9c098b660ca5f02ab3793a4fc1ed8b6c4f8a4
WindSkull <zychm@wit.edu.pl>
2018-07-15 16:14:36 +0200

    Some important changes in DataitemsAtribute

--- MyHero_Core/src/MyHero_Core/DataManagment/DataItemsAtribute.java
+++ MyHero_Core/src/MyHero_Core/DataManagment/DataItemsAtribute.java
@@ -8,13 +8,6 @@ import MyHero_Core.Core.Data;
 import MyHero_Core.Core.DataTemplate;
 import MyHero_ItemAtribute.AtributeType.Atribute;
 import MyHero_ItemAtribute.AtributeType.AtributeGetterInterface;
-import MyHero_ItemAtribute.AtributeType.AtributeName;
-import MyHero_ItemAtribute.Atributes.Atribute_Armor;
-import MyHero_ItemAtribute.Atributes.Atribute_ArmorPenetration;
-import MyHero_ItemAtribute.Atributes.Atribute_Crit;
-import MyHero_ItemAtribute.Atributes.Atribute_Damage;
-import MyHero_ItemAtribute.Atributes.Atribute_Health;
-import MyHero_ItemAtribute.Atributes.Atribute_LifeSteal;
 import MyHero_ItemsAtribute.Core.MyHeroPlayerItemAtributes;
 import MyHero_ItemsAtribute.Managers.ItemsAtributeManager;
 import cn.nukkit.Player;
@@ -34,18 +27,12 @@ public class DataItemsAtribute extends DataTemplate{
 	
 	public DataItemsAtribute(Data maindata) {
 		super(maindata);
-		
-		AllAtributesCreators.put(AtributeName.Damage.toString(), (s) -> new Atribute_Damage(s));
-		AllAtributesCreators.put(AtributeName.Armor.toString(), (s) -> new Atribute_Armor(s));
-		AllAtributesCreators.put(AtributeName.Crit.toString(), (s) -> new Atribute_Crit(s));
-		//AllAtributesCreators.put(AtributeName.Dodge, (s) -> new Atribute_Dodge(s));
-		AllAtributesCreators.put(AtributeName.ArmorPenetration, (s) -> new Atribute_ArmorPenetration(s));
-		AllAtributesCreators.put(AtributeName.Health.toString(), (s) -> new Atribute_Health(s));
-		AllAtributesCreators.put(AtributeName.LifeSteal.toString(), (s) -> new Atribute_LifeSteal(s));
-		
-		
-	}
 
+	}
+	public HashMap<String,AtributeGetterInterface> getAtributeCreaotrs()
+	{
+		return AllAtributesCreators;
+	}
 	public Atribute getAtributeClass(String name,HashMap<String,Double> val)
 	{
 		return AllAtributesCreators.get(name).get(val);
